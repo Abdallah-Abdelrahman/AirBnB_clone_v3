7925f1507dc9802bf4ebfb5225a3446f3916b58e
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-29 19:37:24 +0300

    Elevate the import

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,7 +5,6 @@ Attrs:
     app_views: instance of `Blueprint`
 '''
 from flask import Blueprint
+from api.v1.views.index import *
 
 app_views = Blueprint('app_view', __name__, url_prefix='/api/v1')
-
-from api.v1.views.index import *
