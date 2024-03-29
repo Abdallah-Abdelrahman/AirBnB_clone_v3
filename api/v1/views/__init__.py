b6e06f4a324706edf68d7e424466b7a7baed2363
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-29 23:13:13 +0300

    Add `states` REST api blueprint

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,10 +4,12 @@
 Attrs:
     app_views: instance of `Blueprint`
 '''
+
 from flask import Blueprint
 
 
-app_views = Blueprint('app_view', __name__)
+app_views = Blueprint('app_views', __name__)
 
 
-from api.v1.views.index import *
+from api.v1.views.index import *  # noqa: E402
+import api.v1.views.states  # noqa: E402
---
+++ api/v1/views/states.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+'''States REST api blueprint'''
+from api.v1.views import app_views
+from flask import jsonify, abort, request
+from models import storage
+from models.state import State
+
+
+@app_views.route('/states', strict_slashes=False)
+def get_states():
+    '''Route to retreive all states'''
+    return jsonify([s.to_dict() for s in storage.all(State).values()])
+
+
+@app_views.route('/states/<string:state_id>', strict_slashes=False)
+def get_state_by_id(state_id):
+    '''Route to retreive state by id'''
+    state = storage.get(State, state_id)
+    if not state:
+        return jsonify({'error': 'Not found'}), 404
+    return state.to_dict()
+
+
+@app_views.route('/states/<state_id>',
+                 strict_slashes=False, methods=['DELETE'])
+def del_state_by_id(state_id):
+    '''Route to delete state by id'''
+    state = storage.get(State, state_id)
+    if not state:
+        return jsonify({'error': 'Not found'}), 404
+    state.delete()
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route('/states', strict_slashes=False, methods=['POST'])
+def add_state():
+    '''Route to add new state'''
+    if not request.is_json:
+        return jsonify('Not a JSON'), 400
+    state = request.get_json()
+    if 'name' not in state:
+        return jsonify('Missing name'), 400
+    state = State(**state)
+    state.save()
+
+    return state.to_dict(), 201
+
+
+@app_views.route('/states/<state_id>', strict_slashes=False, methods=['PUT'])
+def update_state_by_id(state_id):
+    '''Route to udpate state based on id'''
+    if not request.is_json:
+        return jsonify('Not a JSON'), 400
+    try:
+        data = request.get_json()
+        state = storage.get(State, state_id)
+        if not state:
+            return jsonify({'error': 'Not found'}), 404
+        for k, v in data.items():
+            if k not in ('id', 'created_at', 'updated_at'):
+                setattr(state, k, v)
+        state.save()
+        return state.to_dict(), 200
+    except Exception:
+        return jsonify('Not a JSON'), 400
