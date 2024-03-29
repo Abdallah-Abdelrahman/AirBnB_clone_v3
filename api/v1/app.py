a6bdcd446a6f025f72a2ed8e3907c73804ba8cac
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-30 00:57:20 +0300

    Use `environ`

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,7 +7,7 @@ Attrs:
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
-from os import getenv
+from os import getenv, environ
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -29,6 +29,10 @@ def page_not_found(e):
 
 
 if __name__ == '__main__':
-    port = getenv('HBNB_API_PORT', 5000)
-    app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(port), threaded=True)
+    host = environ['HBNB_API_HOST']
+    port = environ['HBNB_API_PORT']
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = 5000
+    app.run(host=host, port=port, threaded=True)
