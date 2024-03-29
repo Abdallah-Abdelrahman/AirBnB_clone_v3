cec7f5de2139a0877a5a69b957067cb7740fbb92
y4h14 <yahyasadig2013@gmail.com>
2024-03-29 20:22:41 +0300

    Added 404 errror handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,7 @@
 Attrs:
     app: application that run flask wsgi
 '''
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -20,6 +20,11 @@ def teardown_db(exception):
         exception:
     '''
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return jsonify({"error": "Not found"})
 
 
 if __name__ == '__main__':
