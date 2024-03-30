94cd5cca523497ce565f5a56b0af8bd51e387161
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-30 16:57:32 +0300

    Update users 000

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -37,36 +37,35 @@ def delete_user(user_id):
 def add_user():
     '''creates a new user object'''
     if not request.is_json:
-        return jsonify('Not a JSON'), 400
-    try:
-        data = request.get_json(force=True)
-        if 'email' not in data:
-            return jsonify('Missing email'), 400
-        if 'password' not in data:
-            return jsonify('Missing password'), 400
-        new_user = User(**data)
-        new_user.save()
+        abort(400, 'Not a JSON')
 
-        return jsonify(new_user.to_dict()), 201
-    except Exception:
-        return jsonify('Not a JSON'), 400
+    data = request.get_json()
+    if data is None:
+        abort(400, 'Not a JSON')
+    if 'email' not in data:
+        abort(400, 'Missing email')
+    if 'password' not in data:
+        abort(400, 'Missing password')
+    new_user = User(**data)
+    new_user.save()
+
+    return jsonify(new_user.to_dict()), 201
 
 
 @app_views.route('/users/<user_id>', strict_slashes=False, methods=['PUT'])
 def update_user(user_id):
     '''Updates a User object'''
     if not request.is_json:
-        return jsonify('Not a JSON'), 400
-    try:
-        data = request.get_json(force=True)
-        user = storage.get(User, user_id)
-        if not user:
-            abort(404)
-        for k, v in data.items():
-            if k not in ('id', 'email', 'created_at',
-                         'updated_at'):
-                setattr(user, k, v)
-        user.save()
-        return jsonify(user.to_dict()), 200
-    except Exception:
-        return jsonify('Not a JSON'), 400
+        abort(400, 'Not a JSON')
+    data = request.get_json()
+    if data is None:
+        abort(400, 'Not a JSON')
+    user = storage.get(User, user_id)
+    if not user:
+        abort(404)
+    for k, v in data.items():
+        if k not in ('id', 'email', 'created_at',
+                     'updated_at'):
+            setattr(user, k, v)
+    user.save()
+    return jsonify(user.to_dict()), 200
