672e1122de1a05670ae808b9c79db4662d6b6abf
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-30 16:32:49 +0300

    Update states 002

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -9,7 +9,8 @@ from models.state import State
 @app_views.route('/states', strict_slashes=False, methods=['GET'])
 def get_states():
     '''Route to retreive all states'''
-    return jsonify([s.to_dict() for s in storage.all(State).values()]), 200
+    resp = [s.to_dict() for s in storage.all(State).values()]
+    return jsonify(resp), 200
 
 
 @app_views.route('/states/<string:state_id>',
@@ -37,43 +38,33 @@ def del_state_by_id(state_id):
 @app_views.route('/states', strict_slashes=False, methods=['POST'])
 def add_state():
     '''Route to add new state'''
-    if not request.is_json:
-        abort(400, 'Not a JSON')
     if request.content_type != 'application/json':
         abort(400, 'Not a JSON')
-    try:
-        data = request.get_json()
-        if data is None:
-            abort(400, 'Not a JSON')
-        if 'name' not in data:
-            abort(400, 'Missing name')
-        state = State(**data)
-        state.save()
-
-        return jsonify(state.to_dict()), 201
-    except Exception:
+    data = request.get_json()
+    if data is None:
         abort(400, 'Not a JSON')
+    if 'name' not in data:
+        abort(400, 'Missing name')
+    state = State(**data)
+    state.save()
+
+    return jsonify(state.to_dict()), 201
 
 
 @app_views.route('/states/<string:state_id>',
                  strict_slashes=False, methods=['PUT'])
 def update_state_by_id(state_id):
     '''Route to udpate state based on id'''
-    if not request.is_json:
-        abort(400, 'Not a JSON')
     if request.content_type != 'application/json':
         abort(400, 'Not a JSON')
-    try:
-        data = request.get_json()
-        if data is None:
-            abort(400, 'Not a JSON')
-        state = storage.get(State, state_id)
-        if not state:
-            abort(404)
-        for k, v in data.items():
-            if k not in ('id', 'created_at', 'updated_at'):
-                setattr(state, k, v)
-        state.save()
-        return jsonify(state.to_dict()), 200
-    except Exception:
+    data = request.get_json()
+    if data is None:
         abort(400, 'Not a JSON')
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+    for k, v in data.items():
+        if k not in ['id', 'created_at', 'updated_at']:
+            setattr(state, k, v)
+    state.save()
+    return jsonify(state.to_dict()), 200
