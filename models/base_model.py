16665349db9a6f2ff46cfacedafd4a080bfa4f9e
y4h14 <yahyasadig2013@gmail.com>
2024-03-31 02:44:52 +0300

    Added MD5 hashing to User passwords

--- models/base_model.py
+++ models/base_model.py
@@ -44,10 +44,15 @@ class BaseModel:
             dictionary representaion of class attributes,
                 with `__class__` attr to manifest class instance
         '''
+        from models import db
         _dict = {k: v.isoformat() if isinstance(v, datetime) else
                  v for k, v in self.__dict__.items()
                  if k != '_sa_instance_state'}
         _dict['__class__'] = self.__class__.__name__
+
+        if db:
+            if 'password' in _dict:
+                del _dict['password']
         return _dict
 
     def delete(self):
--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''Module defines `User` class'''
 
+import hashlib
 from models.base_model import BaseModel, Base, store
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
@@ -26,3 +27,11 @@ class User(BaseModel, Base):
         last_name(str):
     '''
     __tablename__ = 'users'
+
+    def __setattr__(self, name, value):
+        '''hases the passwrod of the user'''
+        pwd = hashlib.md5()
+        if name == 'password':
+            pwd.update(value.encode())
+            value = pwd.hexdigest()
+        super().__setattr__(name, value)
