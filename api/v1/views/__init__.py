9d92521f977a37fa4a9b5cc89c62d4abe6e03ad4
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-31 01:28:23 +0300

    Debug 000-3

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -7,15 +7,13 @@ Attrs:
 
 from flask import Blueprint
 
-
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-
-from api.v1.views.index import *  # noqa: E402
-from api.v1.views.states import *  # noqa: E402
-from api.v1.views.cities import *  # noqa: E402
-from api.v1.views.amenities import *  # noqa: E402
-from api.v1.views.users import *  # noqa: E402
-from api.v1.views.places import *  # noqa: E402
-from api.v1.views.places_reviews import *  # noqa: E402
-from api.v1.views.places_amenities import *  # noqa: E402
+from api.v1.views.index import *
+import api.v1.views.states
+import api.v1.views.cities
+import api.v1.views.amenities
+import api.v1.views.users
+import api.v1.views.places
+import api.v1.views.places_reviews
+import api.v1.views.places_amenities
