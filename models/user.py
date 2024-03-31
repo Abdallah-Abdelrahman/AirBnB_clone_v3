3010c3735a6aef1efd1de6f24cb495f1c5f3e8ea
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-03-31 07:15:19 +0300

    Update hashing the password

--- models/user.py
+++ models/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 '''Module defines `User` class'''
 
-import hashlib
+from hashlib import md5
 from models.base_model import BaseModel, Base, store
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
@@ -28,10 +28,10 @@ class User(BaseModel, Base):
     '''
     __tablename__ = 'users'
 
-    def __setattr__(self, name, value):
-        '''hases the passwrod of the user'''
-        pwd = hashlib.md5()
-        if name == 'password':
-            pwd.update(value.encode())
-            value = pwd.hexdigest()
-        super().__setattr__(name, value)
+    def __init__(self, *args, **kw):
+        '''Initialize the instance'''
+        super().__init__(*args, **kw)
+        if 'password' in kw:
+            pwd = md5()
+            pwd.update(kw['password'].encode())
+            self.password = pwd.hexdigest()
