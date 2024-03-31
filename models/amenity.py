5c5c56276d3d4a83e5bbc52217a4f0d955ecb79c
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-04-01 00:41:22 +0300

    Use wrapper functions

--- models/amenity.py
+++ models/amenity.py
@@ -6,12 +6,6 @@ from models.base_model import BaseModel, Base, store
 from sqlalchemy import Column, String
 
 
-@store('place_amenities',
-       name=(Column(String(128), nullable=False), ''),
-       place_amenities=(relationship("Place",
-                        secondary="place_amenity",
-                        back_populates="amenities"),)
-       )
 class Amenity(BaseModel, Base):
     '''Amenity class.
 
@@ -19,3 +13,11 @@ class Amenity(BaseModel, Base):
         name: string
     '''
     __tablename__ = 'amenities'
+
+
+Amenity = store('place_amenities',
+                name=(Column(String(128), nullable=False), ''),
+                place_amenities=(relationship("Place",
+                                              secondary="place_amenity",
+                                              back_populates="amenities"),)
+                )(Amenity)
--- models/place.py
+++ models/place.py
@@ -16,26 +16,6 @@ place_amenity = Table('place_amenity', Base.metadata,
                       )
 
 
-@store('reviews', 'amenities',
-       city_id=(Column(String(60), ForeignKey('cities.id'),
-                       nullable=False), ''),
-       user_id=(Column(String(60), ForeignKey('users.id'),
-                       nullable=False), ''),
-       name=(Column(String(128), nullable=False), ''),
-       description=(Column(String(1024)), ''),
-       number_rooms=(Column(Integer, nullable=False, default=0), 0),
-       number_bathrooms=(Column(Integer, nullable=False, default=0), 0),
-       max_guest=(Column(Integer, nullable=False, default=0), 0),
-       price_by_night=(Column(Integer, nullable=False, default=0), 0),
-       latitude=(Column(Float), 0.0),
-       longitude=(Column(Float), 0.0),
-       reviews=(relationship('Review', backref='place',
-                             cascade='all, delete-orphan'), ),
-       amenities=(relationship("Amenity", secondary=place_amenity,
-                               viewonly=False,
-                               back_populates='place_amenities'), []),
-       amenity_ids=([], [])
-       )
 class Place(BaseModel, Base):
     '''Place class.
 
@@ -76,3 +56,25 @@ class Place(BaseModel, Base):
             self.amenity_ids.append(value.id)
         else:
             pass
+
+
+Place = store('reviews', 'amenities',
+              city_id=(Column(String(60), ForeignKey('cities.id'),
+                              nullable=False), ''),
+              user_id=(Column(String(60), ForeignKey('users.id'),
+                              nullable=False), ''),
+              name=(Column(String(128), nullable=False), ''),
+              description=(Column(String(1024)), ''),
+              number_rooms=(Column(Integer, nullable=False, default=0), 0),
+              number_bathrooms=(Column(Integer, nullable=False, default=0), 0),
+              max_guest=(Column(Integer, nullable=False, default=0), 0),
+              price_by_night=(Column(Integer, nullable=False, default=0), 0),
+              latitude=(Column(Float), 0.0),
+              longitude=(Column(Float), 0.0),
+              reviews=(relationship('Review', backref='place',
+                                    cascade='all, delete-orphan'), ),
+              amenities=(relationship("Amenity", secondary=place_amenity,
+                                      viewonly=False,
+                                      back_populates='place_amenities'), []),
+              amenity_ids=([], [])
+              )(Place)
