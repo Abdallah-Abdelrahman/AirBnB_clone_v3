1ff0e2eb8c5b4ab200605cf33ee90b784ef91784
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-04-01 03:08:54 +0300

    Debug checker 013

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-'''app view blueprint
+"""app view blueprint
 
 Attrs:
-    app_views: instance of `Blueprint`
-'''
+    app_views: instance of Blueprint
+"""
 
 from flask import Blueprint
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 if app_views is not None:
     from api.v1.views.amenities import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,14 +1,33 @@
 #!/usr/bin/python3
-'''Index entry for the blueprint.
+"""Index entry for the blueprint.
 The module has 2 endpoints handler one for the /status
 and the other /stats for counting all the instances as json
-'''
+
+Attrs:
+    STATS: mapping for each instance count
+"""
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
-@app_views.route('/status', strict_slashes=False, methods=['GET'])
+STATS = {
+        'amenities': Amenity,
+        'cities': City,
+        'places': Place,
+        'reviews': Review,
+        'states': State,
+        'users': User,
+        }
+
+
+@app_views.route('/status', methods=['GET'])
 def status():
     '''Status route.
 
@@ -18,24 +37,11 @@ def status():
     return jsonify({'status': 'OK'}), 200
 
 
-@app_views.route('/stats')
+@app_views.route('/stats', methods=['GET'])
 def stats():
     '''Route to retrieves the number of each objects by type.
 
     Returns:
         json of all instances count'''
-    from models.state import State
-    from models.city import City
-    from models.place import Place
-    from models.amenity import Amenity
-    from models.review import Review
-    from models.user import User
 
-    return jsonify({
-        'amenities': storage.count(Amenity),
-        'cities': storage.count(City),
-        'places': storage.count(Place),
-        'reviews': storage.count(Review),
-        'states': storage.count(State),
-        'users': storage.count(User),
-        })
+    return jsonify({k: storage.count(v) for k, v in STATS.items()})
