bf5edab6338f2cf85c40a8d4d5ac640d451f9ac1
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-04-01 11:01:11 +0300

    Debug checker 026

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -92,6 +92,6 @@ class DBStorage:
         Returns:
             number of insances of this cls or all the clasess if None
         """
-        if not cls:
-            return len(self.all(cls))
-        return len(self.all())
+        if cls is None:
+            return len(self.all())
+        return len(self.all(cls))
