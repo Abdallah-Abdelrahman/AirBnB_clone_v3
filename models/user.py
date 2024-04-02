d9b01955a0d1b0aa0ed6de5d985d72cf2b9f56b0
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2024-04-02 05:14:17 +0300

    Debug checker 066

--- models/user.py
+++ models/user.py
@@ -23,8 +23,13 @@ class User(BaseModel, Base):
         first_name = ""
         last_name = ""
 
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
-        if "password" in kwargs:
-            self.password = md5(kwargs['password'].encode()).hexdigest()
+    def __setattr__(self, name, value):
+        '''overload setattr
+        Args:
+            name: the key
+            value: key's value
+        '''
+        if name == 'password':
+            value = md5(value.encode()).hexdigest()
+
+        return super().__setattr__(name, value)
